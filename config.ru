a9df41a71095f41f68ad1263eed3eb58cbd3da8a
esmooov <twotonmary@gmail.com>
2011-06-06 15:54:11 -0400

    passenger ready

---
+++ config.ru
@@ -0,0 +1,24 @@
+require 'rubygems'
+require File.expand_path(File.dirname(__FILE__) + "/conversation")
+
+set :run, false
+set :environment, :production
+
+log = File.new("/tmp/sinatra.log", "a")
+STDOUT.reopen(log)
+STDERR.reopen(log)
+log.close
+
+
+# use Rack::ResponseHeaders do |headers|
+#     headers['Cache-Control'] = "public, max-age=3600"
+# end
+# 
+# set :root, APP_ROOT
+# set :public, File.join(APP_ROOT, 'public')
+# set :cache_enabled, true
+# set :cache_environment, :production
+# set :cache_output_dir, File.join(APP_ROOT, 'public', 'cache')
+
+
+run Sinatra::Application
---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "conversation"
+set :domain, "50.57.67.98"
+set :user, "ubuntu"
+set :ssh_options, { :forward_agent => true }
+set :scm, :git
+set :branch, 'master'
+set :scm_verbose, true
+set :use_sudo, false
+set :repository,  "git@github.com:tpm/conversation-ad.git"
+default_environment['PATH'] = "/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/lib64/ruby/gems/1.8/bin:/bin:/usr/bin:/usr/local/bin:/usr/lib64/ruby/gems/1.8/bin:/root/bin:/bin:/usr/sbin"
+
+role :app, domain
+role :web, domain
+set :deploy_to, "/var/cap/#{application}"
+
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+default_run_options[:pty] = true
+
+namespace :deploy do
+  task :start, :roles => :app do
+  end
+  
+  task :stop, :roles => :app do
+  end
+  
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+end
