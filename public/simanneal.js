4c315221fa851403cd4f06dceb1422d10c2c2acf
Erik Hin-tone <twotonmary@gmail.com>
2011-07-03 09:13:25 -0400

    working old score refactor

--- public/simanneal.js
+++ public/simanneal.js
@@ -1,5 +1,17 @@
 SA = {}
 SA.datareal = [[3, 2, 3, 4, 3, 2, 4, 2, 2, 1], [1, 2, 3, 4, 2, 2, 3, 2, 2, 1], [3, 3, 4, 2, 4, 4, 4, 2, 2, 4], [2, 3, 2, 4, 4, 1, 4, 4, 4, 1], [3, 4, 2, 3, 3, 4, 3, 4, 3, 3], [3, 2, 4, 2, 2, 1, 3, 4, 3, 4], [4, 4, 2, 1, 1, 1, 4, 3, 4, 1], [3, 4, 2, 4, 1, 3, 2, 3, 4, 3], [3, 1, 1, 2, 4, 4, 2, 3, 1, 3], [4, 1, 2, 3, 3, 2, 3, 2, 4, 1]]
+
+SA.datareala = (function(){
+  var output = [];
+  for (i = 0; i < 50; i++){
+    for (j = 0; j < 50; j++){
+      if (_.isUndefined(output[i])) {output[i] = []}
+      output[i][j] = Math.floor(Math.random() * 4+1);
+    }
+  }
+  return output;
+})();
+
 SA.datatest = [[1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1],[2,2,2,2,2,2,2,2,2,2],[2,2,2,2,2,2,2,2,2,2],[3,3,3,3,3,3,3,3,3,3],[3,3,3,3,3,3,3,3,3,3],[4,4,4,4,4,4,4,4,4,4],[4,4,4,4,4,4,4,4,4,4],[4,4,4,4,4,4,4,4,4,4]]
 SA.twodeepCopy = function(arr){
   var onedeep = _(arr).map(function(r){return r.slice();});
@@ -12,7 +24,7 @@ SA.friends = function(data,cell,ridx,cidx){
       b = ridx < 9 ? data[ridx+1][cidx] : 0,
       ans = _([l,r,t,b]).reduce(function(memo,val){
         var newmemo = memo;
-        if (val === cell){newmemo[0] += 1} else if (val !== 0) {newmemo[1] += 1}
+        if (val === cell){newmemo[0] += 1} else {newmemo[1] += 1}
         return newmemo;
       },[0,0])
       return (ans[0] > 0 ? ans[0]*-1 : ans[1]);
@@ -32,11 +44,12 @@ SA.scoreGen = function(data){
   return total;
 }
 
-SA.randCells = function(){
-  var rowindexa = Math.floor(Math.random()*10),
-      colindexa = Math.floor(Math.random()*10);
-      rowindexb = Math.floor(Math.random()*10),
-      colindexb = Math.floor(Math.random()*10);
+SA.randCells = function(data){
+  var l = data.length;
+      rowindexa = Math.floor(Math.random()*l),
+      colindexa = Math.floor(Math.random()*l);
+      rowindexb = Math.floor(Math.random()*l),
+      colindexb = Math.floor(Math.random()*l);
   return [rowindexa,colindexa,rowindexb,colindexb];
 }
 
@@ -49,23 +62,23 @@ SA.exchangeCells = function(data,rule){
   return newdata;
 }
 
-SA.anneal = function(){
+SA.anneal = function(data){
   var temp = 4,
       i = 0,
-      oldscore = SA.scoreGen(SA.datareal),
+      oldscore = SA.scoreGen(data),
       states = [],
       rule,p,candp,candidate,score,theta,state;
   while(temp > .7){
-    rule = SA.randCells();
+    rule = SA.randCells(data);
     p = Math.random();
-    candidate = SA.exchangeCells(SA.datareal,rule);
+    candidate = SA.exchangeCells(data,rule);
     score = SA.scoreGen(candidate);
     theta = oldscore - score;
     candp = 1/(1+(Math.pow(Math.E,-1*(theta/temp))));
     if (candp > p) {
-      SA.datareal = candidate;
+      data = candidate;
       oldscore = score;
-      state = SA.twodeepCopy(SA.datareal);
+      state = SA.twodeepCopy(data);
       states.push(state);
     }
     temp = temp * Math.pow(Math.E,-.0002);
@@ -82,18 +95,20 @@ SA.palette = {1: "red",
 
 SA.drawState = function(data){
   var canvas = document.getElementById("canvas"),
-      c = canvas.getContext("2d");
-  for (i = 0; i < 10; i++){
-    for (j = 0; j < 10; j++){
+      c = canvas.getContext("2d"),
+      len = data[0].length,
+      size = 600/len;
+  for (i = 0; i < len; i++){
+    for (j = 0; j < len; j++){
       var cell = data[i][j];
       c.fillStyle = SA.palette[cell];
-      c.fillRect(i*40,j*40,40,40);
+      c.fillRect(i*size,j*size,size,size);
     }
   }
 }
 $(function(){
   $('#canvas').click(function(){
-    a = SA.anneal();
+    a = SA.anneal(SA.datareal);
     SA.i = setInterval(function(){var s = a.shift(); SA.drawState(s); if (a.length === 0){clearInterval(SA.i)}},1000/240);
   });
 });
